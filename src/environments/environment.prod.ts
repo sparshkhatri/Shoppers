618522cfd1e55e09ec6e1e039854255e5c561aeb
Sparsh Khatri <34980287+sparshkhatri@users.noreply.github.com>
2019-07-26 19:11:02 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-uYOPvJZaiQEhRrY3LMzRpmOxfv9mc-c",
+    authDomain: "shopify-new-25e59.firebaseapp.com",
+    databaseURL: "https://shopify-new-25e59.firebaseio.com",
+    projectId: "shopify-new-25e59",
+    storageBucket: "",
+    messagingSenderId: "75397527151",
+    appId: "1:75397527151:web:bb2cff13a8733e90"
+  }
+};
